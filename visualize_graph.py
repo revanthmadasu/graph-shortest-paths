3c918bd6213060898a465d1fbd7c3839e7b31570
Revanth Madasu <revanthmadasu913@gmail.com>
2023-04-15 20:25:58 -0500

    visualize graph

---
+++ visualize_graph.py
@@ -0,0 +1,18 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+from model.Graph import Graph
+from inputs.input_generation import generate_input_dict
+
+def visualize_graph(graph: Graph):
+    # convert your graph to a networkx graph object
+    nx_graph = nx.Graph(graph.nodeDict)
+
+    # get positions for nodes to use for plotting
+    pos = nx.spring_layout(nx_graph)
+
+    # draw the networkx graph with matplotlib
+    nx.draw(nx_graph, pos, with_labels=True, font_weight='bold')
+    edge_labels = nx.get_edge_attributes(nx_graph, 'weight')
+    nx.draw_networkx_edge_labels(nx_graph, pos, edge_labels=edge_labels, font_color='red')
+    plt.show()
+
