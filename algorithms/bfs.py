e167c95da913d26d4a8acd05b3b31d07896d2589
Revanth Madasu <revanthmadasu913@gmail.com>
2023-04-15 22:28:11 -0500

    optimised bfs

--- algorithms/bfs.py
+++ algorithms/bfs.py
@@ -1,26 +1,30 @@
 from model.Graph import Graph
+from collections import deque
 import time
 def bfs(graph: Graph, source=1):
     start_time = time.time()
 
     visited = set()
     # print(f'Starting bfs traversal with {source}')
-    queue = [source]
+    queue = deque([source])
+    prev = dict()
 
     # starting traversal
-    while len(queue) != 0:
-        cur_node = queue[0]
+    while queue:
+        cur_node = queue.popleft()
         if cur_node in visited:
             # print(f"{cur_node} already visited.")
-            queue.remove(cur_node)
             continue
         # print(f'Visiting {cur_node}')
         visited.add(cur_node)
 
-        queue.extend(graph.get_neighbours(cur_node))
-        queue.remove(cur_node)
+        for neighbour in graph.get_neighbours(cur_node):
+            if neighbour not in visited:
+                queue.append(neighbour)
+                prev[neighbour] = cur_node
+
     end_time = time.time()
     run_time = end_time - start_time
     print("BFS runtime: ", run_time, "seconds")
-    return run_time
+    return {'prev': prev, 'runtime': run_time}
     # print("Traversal done")
