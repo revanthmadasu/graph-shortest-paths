ad24e98cc7be0765ebe19009184835803b7f22fc
Revanth Madasu <revanthmadasu913@gmail.com>
2023-04-15 21:18:18 -0500

    added 40% negative weights

--- inputs/input_generation.py
+++ inputs/input_generation.py
@@ -1,6 +1,7 @@
 import random
 
-def generate_input_dict(num_nodes, directed=True, weighted=True, max_weight=10):
+# random input generator
+def generate_input_dict(num_nodes, directed=True, weighted=True, max_weight=10, negative_weights = False):
     input_dict = {'n': num_nodes, 'direction': 1 if directed else 0, 'weighted': weighted, 'edges': []}
     
     for i in range(1, num_nodes+1):
@@ -8,6 +9,11 @@ def generate_input_dict(num_nodes, directed=True, weighted=True, max_weight=10):
             if random.random() < 0.3:
                 if weighted:
                     weight = random.randint(1, max_weight)
+                    if negative_weights:
+                        # approximately 40% negative weights
+                        if random.randint(1,5) < 3:
+                            weight *= -1
+        
                     input_dict['edges'].append((i, j, weight))
                 else:
                     input_dict['edges'].append((i, j))
@@ -18,6 +24,7 @@ def generate_input_dict(num_nodes, directed=True, weighted=True, max_weight=10):
                         input_dict['edges'].append((j, i))
     return input_dict
 
+# custom user input from cmd
 def cmd_inputs() -> dict:
     weighted = input("Is it a weighted graph: \ny/N").upper() == 'Y'
     direction = input("Select direction: \n1 for Undirected graph \n2 for Directed graph ")
@@ -32,6 +39,3 @@ def cmd_inputs() -> dict:
             v1, v2 = map(int, input().split(" "))
             edges.append((v1, v2))
     return {'n': n, 'e': e, 'edges': edges, 'direction': direction, 'weighted': weighted}
-
-input_dict = generate_input_dict(num_nodes=25, directed=False, weighted=True, max_weight=100)
-print(input_dict)
